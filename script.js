9f3b89a4c1c79de258fa8e44bfed297abe6d386f
gitelmanmarcelo <marcelo.gitelman@gmail.com>
2023-03-20 15:32:33 +0200

    responsive

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ const handleCloseVideoClick = (e) => {
 document.querySelector('#showChessedVideo').addEventListener('click',handleVideoClick);
 document.querySelector('#watchChessedVideoIcon').addEventListener('click',handleVideoClick);
 document.querySelector('#closeVideo').addEventListener('click',handleCloseVideoClick);
+
+document.querySelector('#menuIcon').addEventListener('click', () => {
+  document.querySelector('.dropdown').classList.toggle('active');
+});
+
+document.querySelectorAll('.dropdown a').forEach(el => el.addEventListener('click', () => {
+  document.querySelector('.dropdown').classList.toggle('active');
+}));
